875c2602987fa96885b0ce99587d3907e81f6fb1
Lazynskiy <laz@nordsite.ru>
2016-05-19 17:28:40 +0500

    Solve z4

---
+++ z4/CMakeFiles/format.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/format.dir/format.o"
+  "libformat.pdb"
+  "libformat.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/format.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
